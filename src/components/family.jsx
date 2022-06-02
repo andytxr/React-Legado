a44cf8bd617505bf2270922975ee038521fc9329
decosawa <62211442+andytxr@users.noreply.github.com>
2022-06-02 15:44:54 -0300

    Creating Child Components

---
+++ src/components/family.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Member from './familyMember';
+
+export default props => {
+
+    return(
+    
+        <div>
+            <Member name="Deco" lastName="Teixeira" />
+            <Member name="Elza" lastName="Teixeira" />
+            <Member name="Admilson" lastName="Teixeira" />
+            <Member name="Elza" lastName="Teixeira" />
+        </div>
+
+    )
+
+}
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 import Component from './components/component';
+import Family from "./components/family";
 
 ReactDOM.render(
 
     <div>
         <Component.First/>
         <Component.Second/>
+        <Family/>
     </div>
 
 , document.getElementById('app'));
